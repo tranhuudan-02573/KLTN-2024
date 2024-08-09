ab4de8e3a6e8af1a1773fabb9850684bd2195dec
Tran Huu Dan <20130218@st.hcmuaf.edu.vn>
2024-08-09 23:19:06 +0700

    Update weaviate_rag_non_tenant.py
    update 4

--- src/db_vector/weaviate_rag_non_tenant.py
+++ src/db_vector/weaviate_rag_non_tenant.py
@@ -64,20 +64,27 @@ def create_for_user(document):
     with get_weaviate_client() as client:
         collection = client.collections.create(
             name=document,
-            vector_index_config=wvc.config.Configure.VectorIndex.hnsw(
-                distance_metric=wvc.config.VectorDistances.COSINE,
-                ef_construction=128,
-                cleanup_interval_seconds=300,
-                ef=-1,
-                max_connections=32,
-                dynamic_ef_min=5,
-                dynamic_ef_max=500,
-                dynamic_ef_factor=8,
-                vector_cache_max_objects=1000000,
-                flat_search_cutoff=40000,
-                quantizer=wvc.config.Configure.VectorIndex.Quantizer.bq(
-                    rescore_limit=200,
-                    cache=True,
+            vector_index_config=wvc.config.Configure.VectorIndex.dynamic(
+                distance_metric=weaviate.classes.config.VectorDistances.COSINE,
+                threshold=25000,
+                hnsw=wvc.config.Configure.VectorIndex.hnsw(
+                    ef_construction=128,
+                    cleanup_interval_seconds=300,
+                    ef=-1,
+                    max_connections=32,
+                    dynamic_ef_min=5,
+                    dynamic_ef_max=500,
+                    dynamic_ef_factor=8,
+                    vector_cache_max_objects=1000000,
+                    flat_search_cutoff=40000,
+                    quantizer=wvc.config.Configure.VectorIndex.Quantizer.bq(
+                        rescore_limit=200,
+                        cache=True,
+                    ),
+                ),
+                flat=wvc.config.Configure.VectorIndex.flat(
+                    quantizer=wvc.config.Configure.VectorIndex.Quantizer.bq(cache=True),
+                    vector_cache_max_objects=1000000,
                 ),
             ),
             inverted_index_config=wvc.config.Configure.inverted_index(
